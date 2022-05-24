b886d8e270ecbf056ade27fa58b9ad7cdec629a3
Brian <brian.lockerbie@gmail.com>
2022-05-24 01:13:34 -0700

    added nore Js and moment

---
+++ script.js
@@ -0,0 +1,80 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById
+('current-weather-items');
+
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecaseEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY ='1be9d80034f6359c1e9eab95d987516c'
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth()
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ " " + `<span 
+    id="am-pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+getWeatherData()
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let {latitude, longitude } = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=$
+        {latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=
+        {API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+    
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    currentWeatherItemsEl.innerHTML =
+    `<div class="weather-item">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+        <div>Pressure</div>
+        <div>${pressure}</div>
+    </div>
+    <div class="weather-item">
+        <div>Wind Speed</div>
+        <div>${wind_speed}</div>
+    </div>
+
+    <div class="weather-item">
+        <div>Sunrise</div>
+        <div>${window.moment}</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunset</div>
+        <div>${sunset}</div>
+    </div>
+    
+    `
+
+}
