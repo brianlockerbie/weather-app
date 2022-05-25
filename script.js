613a07dc4fb2d8fd4b54cb1072191c81afc09089
Brian <brian.lockerbie@gmail.com>
2022-05-24 23:07:10 -0700

    added card with search box & some css

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const currentWeatherItemsEl = document.getElementById
 
 const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
-const weatherForecaseEl = document.getElementById('weather-forecast');
+const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const API_KEY ='1be9d80034f6359c1e9eab95d987516c'
 
 setInterval(() => {
     const time = new Date();
-    const month = time.getMonth()
+    const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
@@ -24,8 +24,9 @@ setInterval(() => {
     const minutes = time.getMinutes();
     const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ " " + `<span 
-    id="am-pm">${ampm}</span>`
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : 
+    hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+  ' ' + 
+    `<span id="am-pm">${ampm}</span>`
 
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
 
@@ -34,8 +35,7 @@ setInterval(() => {
 getWeatherData()
 function getWeatherData() {
     navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
-
+        
         let {latitude, longitude } = success.coords;
 
         fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=$
@@ -51,6 +51,9 @@ function getWeatherData() {
 
 function showWeatherData (data){
     let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
 
     currentWeatherItemsEl.innerHTML =
     `<div class="weather-item">
@@ -68,13 +71,42 @@ function showWeatherData (data){
 
     <div class="weather-item">
         <div>Sunrise</div>
-        <div>${window.moment}</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
     </div>
     <div class="weather-item">
         <div>Sunset</div>
-        <div>${sunset}</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
     </div>
     
-    `
+    `;
 
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" 
+             alt="weather-icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.night}&#176; C</div>
+            </div>
+            `
+        }else{
+            otherDayForecast += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')
+                }</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0]
+                .icon}@2x.png" alt="weather-icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.night}&#176; C</div>
+            </div>
+        
+            `
+
+        }
+    })
 }
+
+weatherForecastEl.innerHTML = otherDayForcast;
